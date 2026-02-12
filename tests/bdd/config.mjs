1aa5eff05289f10c7a9358ccfd5e0625c53dfe2a
marijn08 <ma.smorenburg@student.han.nl>
2026-02-12 14:47:46 +0100

    package issues fixed (was in a branch that didnt exist and packages were being annoying when i took changes to other branch)

--- tests/bdd/config.mjs
+++ tests/bdd/config.mjs
@@ -3,12 +3,12 @@ import App from "./page_object/index.js";
 
 export default {
     paths: ["tests/bdd/features/*.feature"],
-    import: [
-        "./node_modules/@qavajs/steps-memory/index.js",
-        "./node_modules/@qavajs/steps-playwright/index.js",
-        "./node_modules/@qavajs/steps-api/index.js",
-        "./tests/bdd/step_definition/*.js" 
-    ],
+    require: [
+        "node_modules/@qavajs/steps-memory/index.js",
+        "node_modules/@qavajs/steps-playwright/index.js",
+        "node_modules/@qavajs/steps-api/index.js",
+        "tests/bdd/step_definition/*.js" 
+    ],  
     format: [
         "@qavajs/console-formatter",
         ["@qavajs/html-formatter", "report/report.html"]
