61599a8cafb9080e5a44ed80aa62321aaa2169b1
Thijmen-Jansen <t.jansen24@student.han.nl>
2026-06-01 19:45:13 +0200

    Fixed the SVG dongle element on the first breadcrumb

--- src/frontend/src/components/BreadcrumbsOverlay.jsx
+++ src/frontend/src/components/BreadcrumbsOverlay.jsx
@@ -8,26 +8,25 @@ export default function BreadcrumbOverlay({}) {
   const withoutMostRecentBreadcrumb = breadcrumbs.slice(0, -1);
 
   return (
-    <div className="opacity-60 hover:opacity-100 flex flex-col items-start">
+    <div className="flex flex-col items-center opacity-60 hover:opacity-100">
       {withoutMostRecentBreadcrumb.length != 0 ? (
-        <div className="max-w-5 max-h-5 ms-20">
+        <div className="-mb-[2px] flex w-50 items-center justify-center">
           <svg
-            viewBox="0 0 100 265"
+            viewBox="8 30 84 175"
             xmlns="http://www.w3.org/2000/svg"
-            className="w-full h-full"
-          >
-            <rect height="100" width="23" y="90" x="38.5" fill="#016630" />
+            className="h-8 w-6"
+            style={{ display: "block" }}>
+            <rect height="115" width="23" y="90" x="38.5" fill="#016630" />
             <ellipse ry="30" rx="30" cy="60" cx="50" fill="#016630" />
           </svg>
         </div>
       ) : null}
-      {withoutMostRecentBreadcrumb.map((breadcrumb) => {
+      {withoutMostRecentBreadcrumb.map((breadcrumb, index) => {
         return (
           <div
             key={breadcrumb.historyId}
-            className="text-green-800 bg-white border-green-700 border-2 rounded-lg p-3 max-w-50 min-w-50 cursor-default last:cursor-pointer mb-2"
-            onClick={() => setCenterNodeId(breadcrumb.originNodeId)}
-          >
+            className={`max-w-50 min-w-50 cursor-default rounded-lg border-2 border-green-700 bg-white p-3 text-green-800 last:cursor-pointer ${index < withoutMostRecentBreadcrumb.length - 1 ? "mb-2" : ""}`}
+            onClick={() => setCenterNodeId(breadcrumb.originNodeId)}>
             {breadcrumb.label}
           </div>
         );
