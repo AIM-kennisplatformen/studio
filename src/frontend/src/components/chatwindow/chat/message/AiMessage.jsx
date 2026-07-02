89be41fa25eb18d78be6b40e69a935764afaacf7
JoeyL0318 <joey.lam2809@gmail.com>
2026-07-02 22:16:53 +0200

    Fully animated message options #249

--- src/frontend/src/components/chatwindow/chat/message/AiMessage.jsx
+++ src/frontend/src/components/chatwindow/chat/message/AiMessage.jsx
@@ -16,26 +16,6 @@ export default function AiMessage({
   feedbackText,
   showFeedback,
 }) {
-  const [copied, setCopied] = useState(false);
-  const timeoutRef = useRef(null);
-
-  async function handleFeedback(
-    messageKey,
-    feedback,
-    setShowFeedback,
-    setFeedbackText
-  ) {
-    setShowFeedback(false);
-    const response = await logResponseFeedback(messageKey, feedback);
-    if (response === null) {
-      setFeedbackText(
-        "An error occurred while sending your feedback. Please try again."
-      );
-      return;
-    }
-    setFeedbackText(response);
-  }
-
   return (
     <div
       key={index}
@@ -60,84 +40,135 @@ export default function AiMessage({
                   "
                 </p>
               )}
-              <Actions>
-                <Action
-                  label="Copy Message"
-                  className="!bg-white hover:!bg-gray-200"
-                  onClick={(e) => {
-                    navigator.clipboard.writeText(value);
-                    setCopied(true);
-                    clearTimeout(timeoutRef.current);
-                    timeoutRef.current = setTimeout(
-                      () => setCopied(false),
-                      2000
-                    );
-                  }}>
-                  <div className="relative flex size-4 items-center justify-center">
-                    <Copy
-                      className={`text-primary absolute size-4 transition-all duration-300 ease-in-out ${
-                        copied
-                          ? "pointer-events-none scale-75 rotate-45 opacity-0"
-                          : "scale-100 rotate-0 opacity-100"
-                      }`}
-                    />
-                    <Check
-                      className={`text-primary absolute size-4 transition-all duration-300 ease-in-out ${
-                        copied
-                          ? "scale-100 rotate-0 opacity-100"
-                          : "pointer-events-none scale-75 -rotate-45 opacity-0"
-                      }`}
-                    />
-                  </div>
-                </Action>
-                {showFeedback ? (
-                  <>
-                    <Action
-                      label="Good response"
-                      className="!bg-white hover:!bg-gray-200"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        handleFeedback(
-                          index,
-                          "positive",
-                          setShowFeedback,
-                          setFeedbackText
-                        );
-                      }}>
-                      <ThumbsUpIcon className="text-primary size-4" />
-                    </Action>
-                    <Action
-                      label="Bad response"
-                      className="!bg-white hover:!bg-gray-200"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        handleFeedback(
-                          index,
-                          "negative",
-                          setShowFeedback,
-                          setFeedbackText
-                        );
-                      }}>
-                      <ThumbsDownIcon className="text-primary size-4" />
-                    </Action>
-                  </>
-                ) : (
-                  <>
-                    <p className="text-sm text-gray-600">{feedbackText}</p>
-                    <a
-                      type="button"
-                      onClick={() => setShowFeedback(true)}
-                      className="text-primary text-sm italic hover:cursor-pointer hover:underline">
-                      Edit Feedback
-                    </a>
-                  </>
-                )}
-              </Actions>
+              <MessageOptions
+                value={value}
+                index={index}
+                setShowFeedback={setShowFeedback}
+                setFeedbackText={setFeedbackText}
+                feedbackText={feedbackText}
+                showFeedback={showFeedback}
+              />
             </div>
           )}
       </div>
     </div>
+  );
+}
+
+function MessageOptions({
+  value,
+  index,
+  setShowFeedback,
+  setFeedbackText,
+  feedbackText,
+  showFeedback,
+}) {
+  const [copied, setCopied] = useState(false);
+  const timeoutRef = useRef(null);
+
+  async function handleFeedback(
+    messageKey,
+    feedback,
+    setShowFeedback,
+    setFeedbackText
+  ) {
+    setShowFeedback(false);
+    const response = await logResponseFeedback(messageKey, feedback);
+    if (response === null) {
+      setFeedbackText(
+        "An error occurred while sending your feedback. Please try again."
+      );
+      return;
+    }
+    setFeedbackText(response);
+  }
+
+  return (
+    <Actions>
+      <Action
+        label="Copy Message"
+        className="!bg-white hover:!bg-gray-200"
+        onClick={(e) => {
+          navigator.clipboard.writeText(value);
+          setCopied(true);
+          clearTimeout(timeoutRef.current);
+          timeoutRef.current = setTimeout(() => setCopied(false), 2000);
+        }}>
+        <div className="relative flex size-4 items-center justify-center">
+          <Copy
+            className={`text-primary absolute size-4 transition-all duration-300 ease-in-out ${
+              copied
+                ? "pointer-events-none scale-75 rotate-45 opacity-0"
+                : "scale-100 rotate-0 opacity-100"
+            }`}
+          />
+          <Check
+            className={`text-primary absolute size-4 transition-all duration-300 ease-in-out ${
+              copied
+                ? "scale-100 rotate-0 opacity-100"
+                : "pointer-events-none scale-75 -rotate-45 opacity-0"
+            }`}
+          />
+        </div>
+      </Action>
+      <div className="relative flex min-h-[32px] items-center overflow-hidden">
+        {/* De Thumbs Buttons */}
+        <div
+          className={`flex transition-all duration-300 ease-in-out ${
+            showFeedback
+              ? "dynamic translate-x-0 scale-100 opacity-100"
+              : "pointer-events-none invisible absolute -translate-x-4 scale-95 opacity-0"
+          }`}>
+          <Action
+            label="Good response"
+            className="!bg-white hover:!bg-gray-200"
+            onClick={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              handleFeedback(
+                index,
+                "positive",
+                setShowFeedback,
+                setFeedbackText
+              );
+            }}>
+            <ThumbsUpIcon className="text-primary size-4" />
+          </Action>
+          <Action
+            label="Bad response"
+            className="!bg-white hover:!bg-gray-200"
+            onClick={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              handleFeedback(
+                index,
+                "negative",
+                setShowFeedback,
+                setFeedbackText
+              );
+            }}>
+            <ThumbsDownIcon className="text-primary size-4" />
+          </Action>
+        </div>
+
+        {/* De Feedback Tekst + Edit link */}
+        <div
+          className={`flex items-center gap-2 transition-all duration-300 ease-in-out ${
+            !showFeedback
+              ? "translate-x-0 scale-100 opacity-100"
+              : "pointer-events-none invisible absolute translate-x-4 scale-95 opacity-0"
+          }`}>
+          <p className="text-sm whitespace-nowrap text-gray-600">
+            {feedbackText}
+          </p>
+          <a
+            type="button"
+            onClick={() => setShowFeedback(true)}
+            className="text-primary text-sm whitespace-nowrap italic select-none hover:cursor-pointer hover:underline">
+            Edit Feedback
+          </a>
+        </div>
+      </div>
+    </Actions>
   );
 }
