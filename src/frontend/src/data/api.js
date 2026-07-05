35cc5805940ecbe0b944f9676fb31fe83fcb56bc
JoeyL0318 <joey.lam2809@gmail.com>
2026-07-05 10:39:00 +0200

    Removed commented code #249

--- src/frontend/src/data/api.js
+++ src/frontend/src/data/api.js
@@ -50,30 +50,6 @@ export async function logResponseFeedback(key, feedback) {
   }
 }
 
-// export async function logSelectedNode(node) {
-//   const url = `${BACKEND_BASE_URL}/log_event`;
-
-//   try {
-//     const response = await fetch(url, {
-//       method: "POST",
-//       credentials: "include",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify({
-//         name: "node_selected",
-//         metadata: {
-//           nodeId: node.id,
-//           nodeLabel: node.data.label,
-//         },
-//       }),
-//     });
-//     if (!response.ok) {
-//       console.error("Failed to log selected node:", response.status);
-//     }
-//   } catch (err) {
-//     console.error("Failed to log selected node:", err);
-//   }
-// }
-
 export async function logEvent(name, metadata) {
   const url = `${BACKEND_BASE_URL}/log_event`;
 
