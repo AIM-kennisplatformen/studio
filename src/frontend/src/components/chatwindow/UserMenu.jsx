6c5ab81a93e0b56653b1899a942a81dd5f922748
JoeyL0318 <joey.lam2809@gmail.com>
2026-08-17 16:49:45 +0200

    Added titles to settings menu

--- src/frontend/src/components/chatwindow/UserMenu.jsx
+++ src/frontend/src/components/chatwindow/UserMenu.jsx
@@ -23,12 +23,14 @@ export default function UserMenu({ setSettingsOpen }) {
       icon: <Settings />,
       action: () => setSettingsOpen(true),
       textColor: "!text-black",
+      title: "Open settings",
     },
     {
       label: "Log Out",
       icon: <LogOut />,
       action: () => logOut(),
       textColor: "!text-red-700",
+      title: "", //Show no tooltip
     },
   ];
 
@@ -42,11 +44,11 @@ export default function UserMenu({ setSettingsOpen }) {
           title="Open menu"
           aria-label="Toggle settings menu">
           <span className="sr-only">Settings</span>
-          <Menu />
+          <Menu className="text-primary size-6 transition-transform duration-300" />
         </button>
         {isOpen && (
           <div className="absolute right-0 z-10 mt-6 w-48 origin-top-right">
-            <Menu menuOptions={menuOptions} />
+            <MenuContent menuOptions={menuOptions} />
           </div>
         )}
       </div>
@@ -54,7 +56,7 @@ export default function UserMenu({ setSettingsOpen }) {
   );
 }
 
-function Menu({ menuOptions }) {
+function MenuContent({ menuOptions }) {
   return (
     <div className="absolute left-0 z-20 w-48 rounded-md border border-gray-200 bg-white shadow-lg">
       {menuOptions.map((option, index) => (
@@ -68,6 +70,7 @@ function MenuButton({ option }) {
   return (
     <button
       onClick={option.action}
+      title={option.title}
       className={`flex w-full items-center rounded !bg-white text-sm hover:!bg-gray-100 ${option.textColor} transition duration-150`}>
       <div className="mr-3">{option.icon}</div>
       {option.label}
